5c3a2c2aafd8a15902f1c3797940666461f24078
Daniel Salazar <dnlsalazar27@gmail.com>
2019-11-19 18:35:00 -0400

    Added JS

--- javascript.js
+++ javascript.js
@@ -1,7 +1,51 @@
+var container = document.getElementById("container");
+
 window.addEventListener("load", function () {
-  window.addEventListener("keyup", move)
+  container.style.setProperty("--Xdeg", 0 + "deg");
+  container.style.setProperty("--Ydeg", 0 + "deg");
+  window.addEventListener("keydown", move);
+  // let value = container.style.getPropertyValue("--Xdeg").slice(0,
+  //   container.style.getPropertyValue("--Xdeg").length - 3);
+  // console.log(++value);
+  // console.log(container.style.getPropertyValue("--Ydeg"));
 })
 
 var move = (e) => {
-  console.log(e);
+  // Up 38
+  // Down 40
+  // Right 39
+  // Left 37
+  const key = e.keyCode? e.keyCode : e.target.getAttribute("dir");
+  let value, num;
+  switch (key) {
+    case 38:
+      value = container.style.getPropertyValue("--Xdeg");
+      num = parseInt(value.slice(0, value.length - 3));
+      num += 5;
+      container.style.setProperty("--Xdeg", num + "deg");
+    break;
+
+    case 40:
+      value = container.style.getPropertyValue("--Xdeg");
+      num = parseInt(value.slice(0, value.length - 3));
+      num -= 5;
+      container.style.setProperty("--Xdeg", num + "deg");
+    break;
+
+    case 39:
+      value = container.style.getPropertyValue("--Ydeg");
+      num = parseInt(value.slice(0, value.length - 3));
+      num += 5;
+      container.style.setProperty("--Ydeg", num + "deg");
+    break;
+
+    case 37:
+      value = container.style.getPropertyValue("--Ydeg");
+      num = parseInt(value.slice(0, value.length - 3));
+      num -= 5;
+      container.style.setProperty("--Ydeg", num + "deg");
+    break;
+  }
+  console.log(value);
+  console.log(num)
 }
